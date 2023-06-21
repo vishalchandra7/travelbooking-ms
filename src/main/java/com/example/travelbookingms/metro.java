76453d65719d9d444dfa349d091e632230ae96d5
unknown <vishal.chandra7@gmail.com>
2023-06-21 22:51:01 +0530

    adding metro class file

---
+++ src/main/java/com/example/travelbookingms/metro.java
@@ -0,0 +1,16 @@
+package com.example.travelbookingms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+
+public class metro {
+    @GetMapping("/mymetro")
+    public String getData() {
+        return "Please book your air ticket from metro station booking counter";
+
+    }
+}
