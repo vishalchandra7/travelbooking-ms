83cebfd8e67460633ce2c9a3abd77361ce40d077
unknown <vishal.chandra7@gmail.com>
2023-08-22 08:48:13 +0530

    adding new class car

---
+++ src/main/java/com/example/travelbookingms/car.java
@@ -0,0 +1,13 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+
+public class car {
+    @GetMapping("/mycar")
+    public String getData() {
+        return "Please book your car ticket";
+    }
+}
