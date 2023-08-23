4511e94e3f1ce7c42bfa15f75fd4b19b6a2702e8
unknown <vishal.chandra7@gmail.com>
2023-08-23 21:11:30 +0530

    adding package

--- src/main/java/com/example/travelbookingms/car.java
+++ src/main/java/com/example/travelbookingms/car.java
@@ -1,3 +1,5 @@
+package com.example.travelbookingms;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
