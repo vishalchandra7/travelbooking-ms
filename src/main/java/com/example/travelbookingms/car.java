7553673ac9ac7b780c15b5f203185148abd08b88
unknown <vishal.chandra7@gmail.com>
2023-08-23 21:18:15 +0530

    abv

--- src/main/java/com/example/travelbookingms/car.java
+++ src/main/java/com/example/travelbookingms/car.java
@@ -1,15 +1,15 @@
 package com.example.travelbookingms;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+ @RestController
 
-public class car {
+ public class car {
     @GetMapping("/mycar")
     public String getData() {
+
         return "Please book your car ticket";
     }
 }
